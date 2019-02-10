078629a339725bfddae08f7251b1fdf2c709bd12
lorecrafting <raymond.n.luong@gmail.com>
2019-02-10 10:57:41 -1000

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
 import { Router } from '@reach/router';
 
-const App = () => <div>Hell Todo</div>;
+const App = () => (
+  <Router>
+    <Home path="/" />
+    <TaskListByGroup path="group/:groupId" />
+  </Router>
+);
+
+const Home = () => <div>Home</div>;
+
+const TaskListByGroup = () => <div>Task List By group</div>;
+
+export default App;
