0537b0613e4d22f6ff08c9f87085fff85df56e84
lorecrafting <raymond.n.luong@gmail.com>
2019-02-10 11:55:10 -1000

    remove navigation helper func in favor of inline func

--- src/pages/Home/GroupCard.jsx
+++ src/pages/Home/GroupCard.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { navigate } from '@reach/router';
 
 const GroupCard = ({ group }) => {
-  const navigateToGroup = groupId => {
-    navigate(`/group/${groupId}`);
-  };
   return (
-    <div onClick={() => navigateToGroup(group.group_id)}>{group.name}</div>
+    <div onClick={() => navigate(`/group/${group.group_id}`)}>{group.name}</div>
   );
 };
 
