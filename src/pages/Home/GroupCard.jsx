f4e3f848726cded8c05494891c74d32782b4958c
lorecrafting <raymond.n.luong@gmail.com>
2019-02-10 13:38:30 -1000

    add progress count text and styles to GroupCard component

--- src/pages/Home/GroupCard.jsx
+++ src/pages/Home/GroupCard.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { navigate } from '@reach/router';
 
 const GroupCard = ({ group }) => {
+  const progressCountText = () => {
+    const totalTasksCount = group.tasks.length;
+    const tasksDoneCount = group.tasks.filter(task => task.completedAt).length;
+    return `${tasksDoneCount} OF ${totalTasksCount} TASKS COMPLETE`;
+  };
   return (
-    <div onClick={() => navigate(`/group/${group.group_id}`)}>{group.name}</div>
+    <div
+      className="clickable-container bottom-border pointer"
+      onClick={() => navigate(`/group/${group.group_id}`)}
+      key={group.group_id}
+    >
+      {group.name}
+      {progressCountText(group.group_id)}
+    </div>
   );
 };
 
