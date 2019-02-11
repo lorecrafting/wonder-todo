34d773fc8dc669970318c4367cbd35f38663f472
lorecrafting <raymond.n.luong@gmail.com>
2019-02-10 14:47:18 -1000

    add task card component

--- src/pages/TaskListByGroup/TaskCard.jsx
+++ src/pages/TaskListByGroup/TaskCard.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context.jsx';
 
 const TaskCard = ({ name, taskId, groupId, completedAt, isLocked }) => {
-  return <div>{name}</div>;
+  const { toggleCheckbox } = useContext(AppContext);
+  return (
+    <div className="clickable-container bottom-border">
+      <div
+        className="icon-container pointer"
+        onClick={() => (!isLocked ? toggleCheckbox(groupId, taskId) : null)}
+      >
+        SVGHERE
+      </div>
+      <div
+        className="card-title"
+        style={{ textDecoration: completedAt ? 'line-through' : 'none' }}
+      >
+        {name}
+      </div>
+    </div>
+  );
 };
 
 export default TaskCard;
