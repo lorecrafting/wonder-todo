005ca9f90a18f4bc5d3515c0d272cc96babdf4eb
lorecrafting <raymond.n.luong@gmail.com>
2019-02-10 14:45:40 -1000

    style task list header

--- src/pages/TaskListByGroup/TaskListByGroup.jsx
+++ src/pages/TaskListByGroup/TaskListByGroup.jsx
@@ -10,9 +10,14 @@ const TaskListByGroup = ({ groupId }) => {
     <div className="flex-container">
       <div className="left-flex-filler" />
       <div className="middle-flex-content">
-        {group.name} <span onClick={() => navigate('/')}>ALL GROUPS</span>
+        <div className="flex-container bottom-border space-between">
+          <div className="header">{group.name}</div>
+          <div className="all-groups-button" onClick={() => navigate('/')}>
+            ALL GROUPS
+          </div>
+        </div>
         {group.tasks.map(task => (
-          <TaskList task={task} groupId={group.group_id} />
+          <TaskList task={task} groupId={group.groupid} />
         ))}
       </div>
       <div className="right-flex-filler" />
